fc32d6e43881a901eaba962a93e133376e840a57
Deividas Sutkus <artiax.code@gmail.com>
2015-09-30 11:24:07 +0100

    Fixed agent shell provisioner

--- PuppetAgent.sh
+++ PuppetAgent.sh
@@ -2,10 +2,10 @@
 apt-get update
 apt-get install -y puppet
 
-echo "$1	$(facter fqdn)	puppetmaster" >> /etc/hosts
+echo "$1	mtmaster.netbuilder.private	puppetmaster" >> /etc/hosts
 echo "127.0.0.1	$(facter fqdn)	puppet" >> /etc/hosts
 echo "$(facter ipaddress_eth1)	$(facter fqdn)	puppet" >> /etc/hosts
 
-printf "[agent]\nserver=$1" >> /etc/puppet/puppet.conf
+printf "[agent]\nserver=mtmaster.netbuilder.private" >> /etc/puppet/puppet.conf
 puppet agent --enable
 puppet agent -tv
