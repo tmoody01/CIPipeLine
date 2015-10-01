3cfc7002cb2c365118bba7be698335f1ceaffca8
Thomas <Thomas.Moody@netbuilder.com>
2015-10-01 11:53:42 +0100

    jenkins untested

---
+++ puppet/modules/jenkins/manifests/install.pp
@@ -0,0 +1,38 @@
+class jenkins::install(
+  $sitelocation = "http://pkg.jenkins-ci.org/redhat/jenkins.repo",
+  $version = "",
+){
+  exec { 'download_jenkins':
+    command => 'wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -',  	
+    }
+	
+  exec { 'echo_jenkins':
+    command => "sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'",
+	require => Exec['download_jenkins'],
+  }
+  
+  exec { 'apt-get update':
+	require => Exec['echo_jenkins'],
+  }
+  
+  package { 'package_jenkins':
+    ensure  => installed,
+	require => Exec['apt-get update'],
+  }
+   
+  service { 'service_jenkins':
+    ensure  => running,
+	require => Package['package_jenkins'],
+  }
+  
+  file_line { 'change_port_jenkins':
+    path   => '/etc/default/jenkins',
+	line   => 'HTTP_PORT=8081',
+    match  => '^HTTP_PORT=',
+	notify => Service['service_jenkins'],
+	require  => Package['package_jenkins'],
+  }
+  
+} # end class
+
+
