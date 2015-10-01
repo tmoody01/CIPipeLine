ea5834ee2f83eaa9c449c46f471d28d1f6434e14
Thomas <Thomas.Moody@netbuilder.com>
2015-10-01 12:32:20 +0100

    vagrant work

--- PuppetAgent.sh
+++ PuppetAgent.sh
@@ -2,9 +2,10 @@
 apt-get update
 apt-get install -y puppet
 
-sudo sed -i "$1"	'mtmaster.netbuilder.private	puppetmaster'  /etc/hosts
-sudo sed -i '127.0.0.1	'"$(facter fqdn)"'	puppet'   /etc/hosts
-sudo sed -i "$(facter ipaddress_eth1)"'	'"$(facter fqdn)"'	puppet'  /etc/hosts
+echo "$1	mtmaster.netbuilder.private	puppetmaster" >> /etc/hosts
+echo "127.0.0.1	$(facter fqdn)	puppet" >> /etc/hosts
+echo "$(facter ipaddress_eth1)	$(facter fqdn)	puppet" >> /etc/hosts
 
 printf "[agent]\nserver=mtmaster.netbuilder.private" >> /etc/puppet/puppet.conf
 puppet agent --enable
+puppet agent -tv
--- PuppetMaster.sh
+++ PuppetMaster.sh
@@ -2,12 +2,11 @@
 apt-get update
 apt-get install -y puppetmaster
 
-sudo sed -i "$(facter ipaddress_eth1)"'	'"$(facter fqdn)"'	puppetmaster'  /etc/hosts
-sudo sed -i '127.0.0.1	'"$(facter fqdn)"'	puppetmaster'  /etc/hosts
+echo "$(facter ipaddress_eth1)	$(facter fqdn)	puppetmaster" >> /etc/hosts
+echo "127.0.0.1	$(facter fqdn)	puppetmaster" >> /etc/hosts
 
 touch /etc/puppet/manifests/site.pp
 
-
-sudo sed -i "*.netbuilder.private" /etc/puppet/autosign.conf
+echo '*.netbuilder.private' >> /etc/puppet/autosign.conf
 
 sudo cp -r /vagrant/puppet/* /etc/puppet/
