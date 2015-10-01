d85dcfabfd5cd088afbb497b45e10e9b097540ed
MatthewTully <tully_m@hotmail.co.uk>
2015-10-01 09:47:25 +0100

    jira plus ip update

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -2,7 +2,7 @@ class java{
   Exec {
     path => [ "/usr/bin", "/bin", "/usr/sbin"]
   }
-  $java_location="http://10.50.20.49:8080/aaron/downloads/jdk-8u45-linux-x64.tar.gz"
+  $java_location="http://10.50.20.18:8080/aaron/downloads/jdk-8u45-linux-x64.tar.gz"
   $java_home="/opt/jdk1.8.0_45/"
   
   include java::install
